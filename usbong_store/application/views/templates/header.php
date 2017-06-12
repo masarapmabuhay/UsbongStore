4c393ff6adfb4c2774fc00792f0c87d070d27cc0
Michael Syson <masarapmabuhay@gmail.com>
2017-06-12 13:35:37 +0800

    +added: categories --> using: bootstrap for the design

--- usbong_store/application/views/templates/header.php
+++ usbong_store/application/views/templates/header.php
@@ -4,6 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
 	<title>Usbong Store Header</title>
 	<style type="text/css">
 
@@ -13,6 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	body {
 		background-color: #fff;
 		margin: 0px auto;
+		margin-top: 10px;
 		max-width: 1024px;
 		font: 16px/24px normal "Helvetica Neue",Helvetica,Arial,sans-serif;
 		color: #808080;
@@ -52,6 +58,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	}
 	
 	.Button {		
+		padding: 5.5px;
 		border-top: 1px solid #d0d0d0;
 		border-right: 1px solid #d0d0d0;
 		border-bottom: 1px solid #d0d0d0;
@@ -68,5 +75,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<input type="text" class="Search-input" placeholder="I'm looking for...">
 		<div class="Button-container"><button type="button" class="Button"><img src="<?php echo base_url('assets/images/magnifying_glass.png'); ?>"></button></div>
     </div>
+	<nav class="navbar navbar">
+	  <div class="container-fluid">
+		<ul class="nav navbar-nav">
+		  <li class="active"><a href="#">BOOKS</a></li>
+		  <li><a href="#">COMBOS</a></li>
+		  <li><a href="#">BEVERAGES</a></li>
+		  <li><a href="#">COMICS</a></li>
+		  <li><a href="#">MANGA</a></li>
+		  <li><a href="#">TOYS & COLLECTIBLES</a></li>
+		  </ul>
+	  </div>
+	</nav>
+	
 </body>
 </html>
