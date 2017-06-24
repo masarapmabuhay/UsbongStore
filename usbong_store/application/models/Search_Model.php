543e636bde02eab514b2f56a82376cba4a67bb86
Michael Syson <masarapmabuhay@gmail.com>
2017-06-24 09:38:56 +0800

    +added: allow search of product name and author if both are included in the search box

--- usbong_store/application/models/Search_Model.php
+++ usbong_store/application/models/Search_Model.php
@@ -3,10 +3,23 @@ class Search_Model extends CI_Model
 {
 	public function getSearchResult($param)
 	{
+		//edited by Mike, 20170624
+		$splitParamArray = explode(" ", $param);		
+	
 		$this->db->select('product_id, name, author, price, product_type_id');
-		$this->db->like('name',$param); 
-		$this->db->or_like('author', $param); 
+		
+		foreach ($splitParamArray as $value) {
+			$this->db->like('name', $value);
+			$this->db->or_like('author', $value);			
+		}
 		$query = $this->db->get('product');
+		
+/*		
+		$this->db->select('product_id, name, author, price, product_type_id');
+		$this->db->like('name', $splitParam); 
+		$this->db->or_like('author', $splitParam); 
+		$query = $this->db->get('product');
+*/		
 		return $query->result_array();
 	}
 }
