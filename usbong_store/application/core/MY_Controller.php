b3f593505d089d45320bc27eed89ea12dae40116
Michael Syson <masarapmabuhay@gmail.com>
2017-07-10 17:44:01 +0800

    +fixed: unable to view product item that uses disallowed characters, e.g. "&", "(", ")", for its name/author

--- usbong_store/application/core/MY_Controller.php
+++ usbong_store/application/core/MY_Controller.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MY_Controller extends CI_Controller {
 
-	public function initStyle() {
+	public function initStyle() {		 		
 		$this->load->view('templates/style');
 	}
 	
