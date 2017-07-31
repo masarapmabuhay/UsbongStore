91762a6b7e388bb2f2de9af358dff65d9523126a
Michael Syson <masarapmabuhay@gmail.com>
2017-07-31 15:06:07 +0800

    +fixed: link from RapidSSL security seal to Website Security page

--- usbong_store/application/views/templates/footer.php
+++ usbong_store/application/views/templates/footer.php
@@ -46,7 +46,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<li><a class="Footer-list-item" href = ""><br></a></li>					
 					<li><span class="Footer-list-header"><b>Quick Help</b></span></li>					
 					<li><a class="Footer-list-item" href = "<?php echo site_url('contact/')?>">Contact Usbong</a></li>					
-					<li><br><a class="Footer-list-item" href = "<?php echo site_url('contact/')?>"><img src="<?php echo base_url('assets/images/RapidSSL_SEAL-90x50.gif'); ?>"></a></li>					
+					<li><br><a class="Footer-list-item" href = "<?php echo site_url('sitesecurity/')?>"><img src="<?php echo base_url('assets/images/RapidSSL_SEAL-90x50.gif'); ?>"></a></li>					
 				</ul>
 			</div>			
 		</div>		
