a16faed6958d017ea527a5d50df0d4d7e948c310
Michael Syson <masarapmabuhay@gmail.com>
2017-08-24 12:32:32 +0800

    +applied: view counter to all the category pages

--- usbong_store/application/models/Childrens_Model.php
+++ usbong_store/application/models/Childrens_Model.php
@@ -14,7 +14,27 @@ class Childrens_Model extends CI_Model
 		
 		$this->db->order_by('name', 'ASEC');	
 		$query = $this->db->get('product');
+		
+		//added by Mike, 20170824
+		$this->incrementViewNum($query->row()->product_type_id);
+		
 		return $query->result_array();
+	}
+	
+	public function incrementViewNum($productTypeId) {
+		$this->db->select('product_type_view_num');
+		$this->db->from('product_type');
+		$this->db->where('product_type_id', $productTypeId);
+		$query = $this->db->get();
+		
+		$viewNum = $query->row()->product_type_view_num;
+		$viewNum++;
+		
+		$updateData = array(
+				'product_type_view_num' => $viewNum
+		);
+		$this->db->where('product_type_id', $productTypeId);
+		$this->db->update('product_type', $updateData);
 	}
 }
 ?>
