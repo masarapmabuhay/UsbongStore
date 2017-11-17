ee70d75eaf484f2bd74bc8983353bdd1ee821815
Michael Syson <masarapmabuhay@gmail.com>
2017-11-17 14:30:29 +0800

    +added: in books page, show quantity sold per product item for admins and merchants who sell the product item --> TODO: update: quantity of product item in DB upon customer purchase --> TODO: apply: feature to all categories

--- usbong_store/application/models/Books_Model.php
+++ usbong_store/application/models/Books_Model.php
@@ -4,7 +4,7 @@ class Books_Model extends CI_Model
 	public function getBooksOnly($merchant_id)
 	{
 		//		$this->db->join('account', 'stories.userid = account.id');
-		$this->db->select('product_id, product_type_id, name, author, price, previous_price, quantity_in_stock, product_view_num');
+		$this->db->select('product_id, product_type_id, name, author, price, previous_price, quantity_in_stock, product_view_num, quantity_sold');
 		$this->db->where('product_type_id','2'); //2 is for type: books
 		
 		if ($merchant_id!=null) {
@@ -20,7 +20,7 @@ class Books_Model extends CI_Model
 	public function getBooks($merchant_id)
 	{
 //		$this->db->join('account', 'stories.userid = account.id');
-		$this->db->select('product_id, product_type_id, name, author, price, previous_price, quantity_in_stock, product_view_num');
+		$this->db->select('product_id, product_type_id, name, author, price, previous_price, quantity_in_stock, product_view_num, quantity_sold');
 		$this->db->where('product_type_id','2'); //2 is for type: books		
 		
 		if ($merchant_id!=null) {
