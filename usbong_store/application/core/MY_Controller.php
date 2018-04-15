bec0a738f7cdaa1876ccc39b28f70d32546e10fe
Michael Syson <masarapmabuhay@gmail.com>
2018-04-15 08:25:04 +0800

    +updated: Usbong Store (web) to determine if the person is using a mobile device --> If yes, redirect him/her to app.usbong.ph, so he/she can download the Android version from the Play Store. --> TODO: update the auto-responsiveness of Usbong Store's UI (User-Interface), so it can display properly even on the iPad and the iPhone

--- usbong_store/application/core/MY_Controller.php
+++ usbong_store/application/core/MY_Controller.php
@@ -6,8 +6,20 @@ class MY_Controller extends CI_Controller {
 	public function initStyle() {		 		
 		$this->load->view('templates/style');
 	}
+
+	//added by Mike, 20180415
+	public function isMobile() {
+//		echo "Hello" . $_SERVER["HTTP_USER_AGENT"];
+		return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+	}
 	
 	public function initHeader() {
+		//edited by Mike, 20180415
+		// If the user is on a mobile device, redirect him/her
+		if($this->isMobile()){
+			header("Location: http://app.usbong.ph");
+		}
+						
 		$customer_id = $this->session->userdata('customer_id');
 
 		if ($customer_id!="") {			
