1bfb13a4782c3ec567b90b81207914b9cb2d8ccb
Michael Syson <masarapmabuhay@gmail.com>
2018-06-12 14:25:19 +0800

    +fixed: in Contact Case Details (Admin) page, message/email body does not properly process and display new lines

--- usbong_store/application/views/contact/contactcasedetailsadmin.php
+++ usbong_store/application/views/contact/contactcasedetailsadmin.php
@@ -40,7 +40,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							echo $contact_case_details->subject;
 							echo '<br><br>';
 //							echo '<b>Description:</b><br>';						
-							echo $contact_case_details->description;	
+							echo nl2br($contact_case_details->description);	
 							echo "<hr class='mail'>";
 							echo '<span class="reply-text">Click here to <a href="mailto:'.$contact_case_details->contact_case_email_address.'">Reply</a></span>';
 						?>
