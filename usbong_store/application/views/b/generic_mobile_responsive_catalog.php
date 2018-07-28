d34be67044a0261a2f663fac8947515c1c95b0e3
Michael Syson <masarapmabuhay@gmail.com>
2018-07-28 08:51:01 +0800

    +fixed: in Merchant page (Mobile Responsive), when the Merchant logo is clicked, the product items are not properly aligned, e.g. no top margin

--- usbong_store/application/views/b/generic_mobile_responsive_catalog.php
+++ usbong_store/application/views/b/generic_mobile_responsive_catalog.php
@@ -59,7 +59,7 @@
 	            </div>
             
                 <div class="row Merchant-category-image">
-                    <a href="<?php echo site_url('b/'.$URLFriendlyReformattedCategoryName.'/'.$this->uri->segment(3)); ?>">
+                    <a href="<?php echo site_url('b/'.strtolower($URLFriendlyReformattedCategoryName).'/'.$this->uri->segment(3)); ?>">
                         <img class="image-responsive center-block" src="<?php echo create_merchant_image_url($merchant->merchant_name); ?>">
                     </a>
                 </div>
