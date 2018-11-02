ac9f1348809582df5a4a27e4d279482ed1b16b03
Michael B. Syson <masarapmabuhay@gmail.com>
2018-11-02 17:34:40 +0800

    +updated: search results page (Non-mobile responsive version) to handle the backspace, delete, left arrow, and right arrow keys when the value of the quantity field already has 2 digits

--- usbong_store/application/views/browse.php
+++ usbong_store/application/views/browse.php
@@ -169,9 +169,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									else {
 								?>											
 								<label class="Quantity-label">Quantity:</label>
+								<!-- edited by Mike, 20181102
+									 keyCodes: 8 is backspace; 46 is delete; 37 is left; 39 is right 
+								-->									
 								<input type="tel" id="quantityId<?php echo $itemCounter.'~'.$resultCount;?>" class="Quantity-textbox no-spin" 
-										name="quantityParam<?php echo $itemCounter;?>"
-										value="1" min="1" max="99" onKeyUp="" onKeyPress="if(this.value.length==2) {return false;} if(parseInt(this.value)<1) {this.value='1'; return false;}" required>					    
+										name="quantityParam<?php echo $itemCounter;?>" value="1" min="1" max="99" onKeyUp="" 
+										onKeyPress="var key = event.keyCode || event.charCode;		
+												const keyBackspace = 8;
+												const keyDelete = 46;
+												const keyLeftArrow = 37;
+												const keyRightArrow = 39;
+
+												if (this.value.length == 2) {			
+													if( key == keyBackspace || key == keyDelete || key == keyLeftArrow || key == keyRightArrow) {
+														return true;
+													}
+													else {
+														return false;										
+													}
+												}" required>				    
 								
 								<input type="hidden" id="productId<?php echo $itemCounter?>" value="<?php echo $value['product_id'];?>">
 								<input type="hidden" id="productName<?php echo $itemCounter?>" value="<?php echo $value['name'];?>">
